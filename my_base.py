5287cce1eb1a8e0b17588dc666ffb8789914da21
jiehunt <jiehunt@hotmail.com>
2017-12-14 18:55:33 +0900

    change to model stacking

--- my_base.py
+++ my_base.py
@@ -5,14 +5,21 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 from sklearn import *
 from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 import xgboost as xgb
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from matplotlib import pyplot as plt
 import codecs
 import string
+from sklearn.linear_model import LogisticRegression
+import time
 
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier
+from sklearn.preprocessing import LabelEncoder
+
+from numba import jit
 
 # pd.set_option('display.height', 1000)
 # pd.set_option('display.max_rows', 500)
@@ -20,6 +27,49 @@ from sklearn.model_selection import GridSearchCV
 # pd.set_option('display.width', 1000)
 
 n_splits = 5
+
+# Evaluate Result
+@jit
+def eval_gini(y_true, y_prob):
+    """
+    Original author CPMP : https://www.kaggle.com/cpmpml
+    In kernel : https://www.kaggle.com/cpmpml/extremely-fast-gini-computation
+    """
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    ntrue = 0
+    gini = 0
+    delta = 0
+    n = len(y_true)
+    for i in range(n - 1, -1, -1):
+        y_i = y_true[i]
+        ntrue += y_i
+        gini += y_i * delta
+        delta += 1 - y_i
+    gini = 1 - 2 * gini / (ntrue * (n - ntrue))
+    return gini
+
+
+def gini_xgb(preds, dtrain):
+    labels = dtrain.get_label()
+    gini_score = -eval_gini(labels, preds)
+    return [('gini', gini_score)]
+
+## from zehhan
+def gini_ze(y, pred):
+    fpr, tpr, thr = metrics.roc_curve(y, pred, pos_label=1)
+    g = 2 * metrics.auc(fpr, tpr) -1
+    return g
+
+def gini_xgb_ze(pred, y):
+    y = y.get_label()
+    return 'gini', gini_ze(y, pred)
+
+def gini_lgb(preds, dtrain):
+    y = list(dtrain.get_label())
+    score = gini(y, preds) / gini(y, y)
+    return 'gini', score, True
+
 
 with codecs.open("./input/salesforse_12132.csv", "r", "Shift-JIS", "ignore") as file:
     train = pd.read_table(file, delimiter=",")
@@ -117,7 +167,6 @@ train[feature_name] = train[feature_name].applymap(lambda x: float(str(x).split(
 train.to_csv("salceforce_freature.csv", index=False)
 train = pd.read_csv("salceforce_freature.csv")
 
-print(train.shape)
 
 ### target encode
 feature_name = ["UW_Status__c"]
@@ -143,7 +192,7 @@ y_train_class = train['UW_Status__c']
 y_train_regre = train['Credit_Line_Size__c']
 train = train.drop(['UW_Status__c', 'Credit_Line_Size__c'], axis=1)
 
-x1, x2, y1, y2 = model_selection.train_test_split(train, y_train_class, test_size=0.15, random_state=99)
+x1, x2, y1, y2 = model_selection.train_test_split(train, y_train_class, test_size=0.1, random_state=99)
 for f in train.columns:
     if train[f].dtype=='object':
         print("here is object", f)
@@ -152,6 +201,8 @@ X_train = x1
 y_train = y1
 X_test = x2
 y_test = y2
+
+print(train.shape)
 
 params = {'eta': 0.1,
           'max_depth': 5,
@@ -249,40 +300,40 @@ xgb_model = XGBClassifier(**params)
 
 
 ##### cross fit
-kf = StratifiedKFold(n_splits=n_splits, random_state=22)
-kf.get_n_splits(X_train, y_train)
-y_preds_train = []
-y_preds_test = []
-for k, (train_idx, valid_idx) in enumerate(kf.split(X_train, y_train)):
+# kf = StratifiedKFold(n_splits=n_splits, random_state=22)
+# kf.get_n_splits(X_train, y_train)
+# y_preds_train = []
+# y_preds_test = []
+# for k, (train_idx, valid_idx) in enumerate(kf.split(X_train, y_train)):
+#
+#     xgb_model.fit(X_train.iloc[train_idx],
+#                   y_train.iloc[train_idx],
+#                eval_set=[(X_train.iloc[valid_idx], y_train.iloc[valid_idx])],
+#                eval_metric='auc',
+#                verbose=False,
+#                early_stopping_rounds=50)
+#
+#     y_pred_train = xgb_model.predict_proba(X_train.iloc[valid_idx])[:,1]
+#     y_pred_test = xgb_model.predict(X_test)
+#     # y_pred_k = np.argmax(y_pred_test, axis=1)
+#     accu = accuracy_score(y_test, y_pred_test)
+#     print('fold[{:>3d}]: accuracy = {:>.4f}'.format(k, accu))
+#     y_preds_train.append(y_pred_train)
+#     y_preds_test.append(y_pred_test)
+#
+#
+# col = [c for c in train.columns]
+# train["my_predict"] = xgb_model.predict(train[col])
+# train["my_predict_proba"] = xgb_model.predict_proba(train[col])[:,1]
+# cross_score = cross_val_score(xgb_model, x2, y2, cv=n_splits, scoring='accuracy')
+# print("    cross_score: %.5f" % (cross_score.mean()))
 
-    xgb_model.fit(X_train.iloc[train_idx],
-                  y_train.iloc[train_idx],
-               eval_set=[(X_train.iloc[valid_idx], y_train.iloc[valid_idx])],
-               eval_metric='auc',
-               verbose=False,
-               early_stopping_rounds=50)
-
-    y_pred_train = xgb_model.predict_proba(X_train.iloc[valid_idx])[:,1]
-    y_pred_test = xgb_model.predict(X_test)
-    # y_pred_k = np.argmax(y_pred_test, axis=1)
-    accu = accuracy_score(y_test, y_pred_test)
-    print('fold[{:>3d}]: accuracy = {:>.4f}'.format(k, accu))
-    y_preds_train.append(y_pred_train)
-    y_preds_test.append(y_pred_test)
-
-
-col = [c for c in train.columns]
-train["my_predict"] = xgb_model.predict(train[col])
-train["my_predict_proba"] = xgb_model.predict_proba(train[col])[:,1]
-cross_score = cross_val_score(xgb_model, x2, y2, cv=n_splits, scoring='accuracy')
-print("    cross_score: %.5f" % (cross_score.mean()))
-
-train_pred = xgb_model.predict(X_train[col])
-accu = accuracy_score(train_pred, y_train)
-print('Train : accuracy = {:>.4f}'.format(accu))
-
-accu = accuracy_score(train["my_predict"], y_train_class)
-print('Total : accuracy = {:>.4f}'.format(accu))
+# train_pred = xgb_model.predict(X_train[col])
+# accu = accuracy_score(train_pred, y_train)
+# print('Train : accuracy = {:>.4f}'.format(accu))
+#
+# accu = accuracy_score(train["my_predict"], y_train_class)
+# print('Total : accuracy = {:>.4f}'.format(accu))
 #
 # importance = xgb_model.feature_importances_
 # features = train.columns
@@ -298,3 +349,119 @@ print('Total : accuracy = {:>.4f}'.format(accu))
 # xgb.plot_importance(xgb_model)
 # plt.show()
 
+###Ensemble Generation
+class Ensemble(object):
+    def __init__(self, n_splits, stacker, base_models):
+        self.n_splits = n_splits
+        self.stacker = stacker
+        self.base_models = base_models
+
+    def fit_predict(self, X, y, T):
+        t_X = X
+        X = np.array(X)
+        y = np.array(y)
+        T = np.array(T)
+
+        folds = list(StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=2016).split(X, y))
+
+        S_train = np.zeros((X.shape[0], len(self.base_models)))
+        S_test = np.zeros((T.shape[0], len(self.base_models)))
+        for i, clf in enumerate(self.base_models):
+
+            S_test_i = np.zeros((T.shape[0], self.n_splits))
+
+            for j, (train_idx, test_idx) in enumerate(folds):
+                X_train = X[train_idx]
+                y_train = y[train_idx]
+                X_holdout = X[test_idx]
+                #                y_holdout = y[test_idx]
+
+                start = time.time()
+                print ("Fit %s fold %d" % (str(clf).split('(')[0], j+1))
+                if str(clf).__contains__("XGB"):
+                    print("goto xgb fit")
+                    clf.fit(X_train, y_train)
+                elif str(clf).__contains__("LGB"):
+                    print("goto lgb fit")
+                    clf.fit(X_train, y_train)
+                else:
+                    clf.fit(X_train, y_train)
+
+                # cross_score = cross_val_score(clf, X_train, y_train, cv=n_splits, scoring='roc_auc')
+                # print("    cross_score: %.5f" % (cross_score.mean()))
+                y_pred = clf.predict_proba(X_holdout)[:,1]
+
+                S_train[test_idx, i] = y_pred
+                S_test_i[:, j] = clf.predict_proba(T)[:,1]
+                print('using time : %5.1f min' % ((time.time() -start ) / 60) )
+            S_test[:, i] = S_test_i.mean(axis=1)
+
+        results = cross_val_score(self.stacker, S_train, y, cv=n_splits, scoring='roc_auc')
+        print("Stacker score: %.5f" % (results.mean()))
+
+        self.stacker.fit(S_train, y)
+        pred = self.stacker.predict_proba(S_train)[:,1]
+        print( "  Total Gini = ", eval_gini(y, pred) )
+
+        res = self.stacker.predict_proba(S_test)[:,1]
+        res_p = self.stacker.predict(S_test)
+
+        return res, res_p
+
+
+# LightGBM params
+lgb_params = {}
+lgb_params['learning_rate'] = 0.02
+lgb_params['n_estimators'] = 650
+lgb_params['max_bin'] = 10
+lgb_params['subsample'] = 0.8
+lgb_params['subsample_freq'] = 10
+lgb_params['colsample_bytree'] = 0.8
+lgb_params['min_child_samples'] = 500
+lgb_params['random_state'] = 99
+lgb_params['device'] = 'gpu'
+lgb_params['gpu_platform_id'] = 0
+lgb_params['gpu_device_id'] = 0
+
+lgb_params2 = {}
+lgb_params2['n_estimators'] = 1090
+lgb_params2['learning_rate'] = 0.02
+lgb_params2['colsample_bytree'] = 0.3
+lgb_params2['subsample'] = 0.7
+lgb_params2['subsample_freq'] = 2
+lgb_params2['num_leaves'] = 16
+lgb_params2['random_state'] = 99
+lgb_params2['device'] = 'gpu'
+lgb_params2['gpu_platform_id'] = 0
+lgb_params2['gpu_device_id'] = 0
+
+lgb_params3 = {}
+lgb_params3['n_estimators'] = 1100
+lgb_params3['max_depth'] = 4
+lgb_params3['learning_rate'] = 0.02
+lgb_params3['random_state'] = 99
+lgb_params3['device'] = 'gpu'
+lgb_params3['gpu_platform_id'] = 0
+lgb_params3['gpu_device_id'] = 0
+
+lgb_model = LGBMClassifier(**lgb_params)
+
+lgb_model2 = LGBMClassifier(**lgb_params2)
+
+lgb_model3 = LGBMClassifier(**lgb_params3)
+
+
+log_model = LogisticRegression()
+
+stack = Ensemble(n_splits=n_splits,
+                 stacker = log_model,
+                 base_models = (lgb_model,lgb_model2,lgb_model3, xgb_model))
+
+y_pred_p,y_pred = stack.fit_predict(X_train, y_train, X_test)
+
+###Output resault
+
+accu = accuracy_score(y_pred, y_test)
+print('Cross : accuracy = {:>.4f}'.format(accu))
+
+#
